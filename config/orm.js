0fbdaeb24ecf8844f52ec9a989c2293d8b6544f1
zrcarter2017 <zcarter@allegiancy.us>
2018-02-11 21:08:08 -0500

    changed orm file to object notation

--- config/orm.js
+++ config/orm.js
@@ -1,31 +1,36 @@
 var connection = require("../config/connection.js");
 
-function selectAll() {
-	var queryString = "SELECT * FROM burgers;"
-	connection.query(queryString, function(err, result) {
-		if (err) {
-			throw err;
-		}
-		return result;
-	});
-}
+var orm = {
 
-function insertOne(userInput) {
-	var queryString = "INSERT INTO burgers (burger_name) VALUES ('" + userInput + "');";
-	connection.query(queryString, function(err, result) {
-		if (err) {
-			throw err;
-		}
-		return result;
-	});
-}
+	selectAll: function(cb) {
+		var queryString = "SELECT * FROM burgers;"
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
 
-function updateOne(id) {
-	var queryString = "UDPATE burgers WHERE id = '" + id + "';";
-	connection.query(queryString, function(err, result) {
-		if (err) {
-			throw err;
-		}
-		return result;
-	})
-}
+	insertOne: function(userInput) {
+		var queryString = "INSERT INTO burgers (burger_name) VALUES ('" + userInput + "');";
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			return result;
+		});
+	},
+
+	updateOne: function(id) {
+		var queryString = "UDPATE burgers SET devoured = true WHERE id = '" + id + "';";
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			return result;
+		})
+	}
+};
+
+module.exports = orm;
