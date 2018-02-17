b41c5fbfca7cb891aca0b348f74e718d608d9950
zrcarter2017 <zcarter@allegiancy.us>
2018-02-16 21:26:00 -0500

    added special methods to orm, added json packages

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,32 @@
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+	return arr.toString();
+}
+
+function objToSql(ob) {
+	var arr = [];
+	for (var key in ob) {
+		var value = ob[key];
+
+		if (Object.hasOwnProperty.call(ob, key)) {
+			if (typeof value==="string" && value.indexOf(" ") >= 0) {
+				value = "" + value + "";
+			}
+		arr.push(key + "=" + value);
+		}
+	}	
+return arr.toString();
+}
+
 var orm = {
 
 	selectAll: function(cb) {
-		var queryString = "SELECT * FROM burgers;"
+		var queryString = "SELECT * FROM" + tableInput + ";";
 		connection.query(queryString, function(err, result) {
 			if (err) {
 				throw err;
@@ -13,7 +36,17 @@ var orm = {
 	},
 
 	insertOne: function(userInput, cb) {
-		var queryString = "INSERT INTO burgers (burger_name) VALUES ('" + userInput + "');";
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+	    queryString += cols.toString();
+	    queryString += ") ";
+	    queryString += "VALUES (";
+	    queryString += printQuestionMarks(vals.length);
+	    queryString += ") ";
+
+	    console.log(queryString);
+
 		connection.query(queryString, function(err, result) {
 			if (err) {
 				throw err;
@@ -23,14 +56,20 @@ var orm = {
 	},
 
 	updateOne: function(id, cb) {
-		var queryString = "UDPATE burgers SET devoured = true WHERE id = '" + id + "';";
-		connection.query(queryString, function(err, result) {
-			if (err) {
-				throw err;
-			}
-			cb(result);
-		})
-	}
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 -- CREATE DATABASE burger_db;
 USE burger_db;
-
+DROP TABLE burgers;
 CREATE TABLE burgers (
 	id INT Primary Key Auto_Increment,
 	burger_name VARCHAR(40) NOT NULL,
 	devoured BOOLEAN DEFAULT 0,
-	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP	
+	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
